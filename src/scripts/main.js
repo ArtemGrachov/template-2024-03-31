7a649225eedb1be866c078c64323ca2acd56b829
Artem Grachov <grachov2195@gmail.com>
2024-04-02 22:43:35 +0300

    Counters section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,56 @@ import { Carousel } from 'bootstrap';
 
 new Carousel('#hero-banner');
 new Carousel('#text-media-gallery-1');
+
+observeCounterRow();
+
+function observeCounterRow() {
+  const couterRowEl = document.querySelector('#counter-row');
+
+  const callback = (entries, observer) => {
+    entries.map((entry) => {
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+
+        const counters = couterRowEl.getElementsByClassName('animated-counter');
+        new Array(...counters).forEach(el => counterAnimation(el));
+      }
+    });
+  }
+
+  const observer = new IntersectionObserver(callback);
+
+  observer.observe(couterRowEl);
+}
+
+/**
+ * @param {Element} target 
+ */
+function counterAnimation(target) {
+  const initial = +(target.dataset.initial ?? 0);
+  const limit = +target.innerText;
+  const direction = target.dataset.direction ?? 'up';
+  const tick = target.dataset.tick ?? 10;
+
+  let counter = initial;
+
+  const interval = setInterval(() => {
+    if (direction === 'down') {
+      counter--;
+
+      if (counter < limit) {
+        clearInterval(interval);
+        return;
+      }
+    } else {
+      counter++;
+
+      if (counter > limit) {
+        clearInterval(interval);
+        return;
+      }
+    }
+
+    target.innerText = counter;
+  }, tick);
+}
